21a805eb37faeef2e3c75d19de95b5800daeb573
giorgio.lasala <giorgio.lasala@avanade.com>
2022-11-26 15:19:20 +0100

    two simple functions

---
+++ src/XMasFunctions.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace XMazeApi
+{
+    public static class XMasFunctions
+    {
+        [FunctionName("DefaultFunction")]
+        public static async Task<IActionResult> RunDefaultFunction(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# RunDefaultFunction");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("XMasTotalPackages")]
+        public static async Task<HttpResponseMessage> RunXMasTotalPackages(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# RunXMasTotalPackages");
+
+            string name = req.Query["name"];
+
+            var html = "<html><head><script>alert('"+ name + "')</script></head><body><span>"+ name + "</span></body></html>";
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(html);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+
+        }
+    }
+}
